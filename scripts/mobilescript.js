08b19ff4b44cd57552b617dc3bc9d0b3cd26e441
Jason Eveleth <jasoneveleth@jasons-mac.lan>
2020-06-06 00:39:48 -0400

    erase website when on iphone

--- scripts/mobilescript.js
+++ scripts/mobilescript.js
@@ -2,6 +2,7 @@ window.addEventListener('load', iOS());
 
 function iOS() {
     if( navigator.userAgent.match(/Android|iPhone/i) !== null ) {
+        document.getElementById('body').innerHTML = '';
         var elem = document.getElementById("text");
         elem.style.marginLeft = "0%";
         elem.style.marginRight = "0%";
