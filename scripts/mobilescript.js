a4693c2c3e646579665af1526634736567178dca
Jason Eveleth <jasoneveleth@jasons-mac.lan>
2020-06-07 01:07:15 -0400

    change to mobil script

--- scripts/mobilescript.js
+++ scripts/mobilescript.js
@@ -1,6 +1,7 @@
-window.addEventListener('load', iOS());
+document.addEventListener("DOMContentLoaded", iOS()}
 
 function iOS() {
+    console.log("yeehaw")
     if( navigator.userAgent.match(/Android|iPhone/i) !== null ) {
         document.getElementById('body').innerHTML = '';
         var elem = document.getElementById("text");
